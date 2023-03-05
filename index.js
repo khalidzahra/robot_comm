89c9c3147c1813a502c240b44a435cca23777f62
khalidzahra <khalidzahra923@gmail.com>
2023-03-05 20:01:28 +0200

    Add server listening message

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const net = require('net');
 const PORT = process.env.PORT || 8000;
 
-const server = net.createServer(function (socket) {
+const server = net.createServer((socket) => {
     socket.on("data", function (data) {
         console.log(data);
     });
 });
-server.listen(PORT);
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
