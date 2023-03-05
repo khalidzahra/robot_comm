a465e189d458b56ed5be595faa46c1b0f7e86fe7
khalidzahra <khalidzahra923@gmail.com>
2023-03-05 20:15:59 +0200

    Add response message to socket communication

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const PORT = process.env.PORT || 8000;
 
 const server = net.createServer((socket) => {
     socket.on("data", function (data) {
-        console.log(data);
+        console.log(data.toString());
+        socket.write(`Received: ${data.toString()}`);
     });
 });
 server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${process.env.address} ${PORT}`);
 });
